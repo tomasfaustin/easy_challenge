0ff807b0574cc79fb625f8c6d8af926ad32294a6
Thomas Huisking <tomasfaustin@gmail.com>
2017-06-16 21:07:03 -0700

    Create readme so you understand the context of the easy challenge

--- main.js
+++ main.js
@@ -1,6 +1,3 @@
-// Input: a bunch of times, minutes and seconds, formatted as a single string like: "12:32 34:01 15:23 9:27 55:22 25:56"
-// Output: the sum of the times, hours, minutes, and seconds, formatted as a single string like: "2:32:41"
-
 function timeString(str) {
   // split string into an array
   str = str.split(' ');
