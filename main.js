2a1f8822b3656d64dc888b117e41ff0f29d97426
Thomas Huisking <tomasfaustin@gmail.com>
2017-06-16 21:00:34 -0700

    Redo hours, minutes, and seconds

--- main.js
+++ main.js
@@ -13,13 +13,14 @@ function timeString(str) {
   // get minutes and seconds
     var min = parseInt(newStr[0]);
     var sec = parseInt(newStr[1]);
-    output += (min * 60) + sec;
+    output += ((min * 60) + sec);
   }
   // get hours, minutes, and seconds
-    var hour = Math.floor(output / 3600);
-    var min  = Math.floor(output % 3600 / 60);
+    var hours = Math.floor(output / 3600);
+    var minutes  = Math.floor((output % 3600) / 60);
     var seconds = Math.floor(output % 60);
 
+    console.log(hours + ":" + minutes + ":" + seconds)
 }
 
 timeString("12:32 34:01 15:23 9:27 55:22 25:56")
