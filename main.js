ba43f24115199cc9302e954d21750918aa0900bb
Thomas Huisking <tomasfaustin@gmail.com>
2017-06-16 20:48:24 -0700

    Split string, create output, add for loop, split strings, get minutes and seconds

--- main.js
+++ main.js
@@ -1,2 +1,21 @@
 // Input: a bunch of times, minutes and seconds, formatted as a single string like: "12:32 34:01 15:23 9:27 55:22 25:56"
 // Output: the sum of the times, hours, minutes, and seconds, formatted as a single string like: "2:32:41"
+
+function timeString(str) {
+  // split string into an array
+  str = str.split(' ');
+  // create output for number conversion
+  var output = 0;
+  // loop through array
+  for (var i = 0; i < str.length; i++) {
+  // split strings at colon
+    var newStr = str[i].split(':');
+  // get minutes and seconds
+    var min = parseInt(newStr[0]);
+    var sec = parseInt(newStr[1]);
+    console.log(sec)
+  }
+
+}
+
+timeString("12:32 34:01 15:23 9:27 55:22 25:56")
